e6cbf75690ccc2f084d2a2948cf8c2456cada9c2
leonardoparra <leonardocabrices1@gmail.com>
2024-02-26 17:48:39 -0400

    ejercicio diario

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :bookmarks, only: :destroy
+  resources :lists do
+    resources :bookmarks
+  end
 end
